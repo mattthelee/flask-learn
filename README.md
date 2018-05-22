35c90dd3611977d9df4036c8fb9dd9e92d8ed65f
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-05-22 08:58:33 +0100

    Adding registration page and real login

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 My first play around with flask following the tutorial here: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 
 #Prerequisites
-```pip install flask flask-wtf flask-sqlalchemy flask-migrate```
+```pip install flask flask-wtf flask-sqlalchemy flask-migrate flask-login```
 
 # Running:
 To run do the following:
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # Flask automatically identifies functions of form validate_<fieldname>
+    # and applies them as custom validators
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('{} is already in use, please choose a different username.'.format(username))
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('That email address is already registered.')
